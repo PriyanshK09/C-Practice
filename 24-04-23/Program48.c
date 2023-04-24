0ad51f24b28a689060c3689511d46466ea57034b
Mr. Priyansh <priyanshkhare4905@gmail.com>
2023-04-24 17:27:01 +0530

    Changes committed

---
+++ 24-04-23/Program48.c
@@ -0,0 +1,27 @@
+// WAP TO DEMONSTRATE THE CONCEPT OF THE STRING
+#include <stdio.h>
+#include <string.h>
+
+// String Length
+// int main()
+// {
+//     char string[]="Hello World";
+//     printf("%d",strlen(string));
+// }
+
+
+// String Size
+// int main()
+// {
+//     char string[]="Hello World";
+//     printf("%d",sizeof(string));
+// }
+
+// String Length 2
+// int main()
+// {
+//     char string[]="Hello World, I am studying about how to print the string length";
+//     printf("%d",strlen(string));
+// }
+
+// String Reverese
