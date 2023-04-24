967df99f8707d696e6aad9b2853740dc7e7bb058
Mr. Priyansh <priyanshkhare4905@gmail.com>
2023-04-24 17:36:53 +0530

    Changes committed

--- 24-04-23/Program48.c
+++ 24-04-23/Program48.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-// String Length
+// - String Length
 // int main()
 // {
 //     char string[]="Hello World";
@@ -17,11 +17,39 @@
 //     printf("%d",sizeof(string));
 // }
 
-// String Length 2
+// - String Length 2
 // int main()
 // {
 //     char string[]="Hello World, I am studying about how to print the string length";
 //     printf("%d",strlen(string));
 // }
 
-// String Reverese
+// - String Reverese
+// int main()
+// {
+//     char string[]="Hello World";
+//     printf("%s",strrev(string));
+// }
+
+// - String LWR (Used for lower case)
+// int main()
+// {
+//     char string[]="Hello World, ALL CAPS WILL BE CHANGED TO LOWER CASE";
+//     printf("%s",strlwr(string));
+// }
+
+// - String UPR (Used for upper case)
+// int main()
+// {
+//     char string[]="Hello World, all caps will be changed to upper case";
+//     printf("%s",strupr(string));
+// }
+
+// - String Copy is used to copy the string (strcopy(destination,source))
+int main()
+{
+    char string[]="Hello World";
+    char string2[20];
+    strcpy(string2,string);
+    printf("%s",string2);
+}
