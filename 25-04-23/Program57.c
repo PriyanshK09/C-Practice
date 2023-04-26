faa4e2deca37c30272cf4b46a74bb479618d216a
Mr. Priyansh <priyanshkhare4905@gmail.com>
2023-04-26 22:43:15 +0530

    Changes committed

--- 25-04-23/Program57.c
+++ 25-04-23/Program57.c
@@ -10,4 +10,13 @@ int main()
     printf("Second element: %d\n", *p); // 20
     p = p + 1;
     printf("Third element: %d\n", *p); // 30
+
+    *p = 45;
+    p = p + 2
+    *p = -2
+    printf("New array is \n");
+    for (i = 0; i < 5; i++)
+    {
+        printf("%d ", arr[i]);
+    }
 }
